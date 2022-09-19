d157806bea20399f9b537e8436b7c2c35c262414
heosuyeon <hsy121891@gmail.com>
2022-09-19 22:07:20 +0900

    번역 진행중

--- miss_university/src/Component/Ranked.js
+++ miss_university/src/Component/Ranked.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../Assets/css/ranked.css";
+import { useTranslation, initReactI18next } from "react-i18next";
 
 const Ranked = ({user}) => {
+  const { t } = useTranslation();
+
   return (
     <div className={user.rank === 1? "rankMedalLayout1" : user.rank === 2? "rankMedalLayout2" : "rankMedalLayout3"}>
       <span className={user.rank === 1? "rankMedalText1": user.rank === 2? "rankMedalText2": "rankMedalText3"}>
-        실시간
+        {t("real_time")}
         <br />
-        <span className="rankMedalNum">{user.rank === 1? "1": user.rank === 2? "2" : "3"}위</span>
+        <span className="rankMedalNum">{user.rank === 1? "1": user.rank === 2? "2" : "3"}{t("num_ranking")}</span>
       </span>
     </div>
   );
