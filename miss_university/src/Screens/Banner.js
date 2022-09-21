67ba31929ae30b5f969c1f05fc23419979b5ea37
hannah <hanna0175@gmail.com>
2022-09-21 10:45:14 +0900

    banner

--- miss_university/src/Screens/Banner.js
+++ miss_university/src/Screens/Banner.js
@@ -1,9 +1,9 @@
 import React from "react";
 
 import "../Assets/css/banner.css";
-import { ReactComponent as SVGBanner1 } from "../Assets/images/text_miss.svg";
-import { ReactComponent as SVGBanner2 } from "../Assets/images/text_university.svg";
-import { ReactComponent as SVGBanner3 } from "../Assets/images/text_world.svg";
+import { ReactComponent as SVGBanner1 } from "../Assets/images/text_world.svg";
+import { ReactComponent as SVGBanner2 } from "../Assets/images/text_miss.svg";
+import { ReactComponent as SVGBanner3 } from "../Assets/images/text_university.svg";
 import i18n from "i18next";
 import { useTranslation, initReactI18next } from "react-i18next";
 
@@ -13,20 +13,28 @@ export const Banner = () => {
     <div id="banner_root">
       <div id="banner_container">
         <div className="left">
-          <SVGBanner1 className="bannerImage1" />
-          <SVGBanner2 className="bannerImage2" />
-          <SVGBanner3 className="bannerImage1" />
-          <span className="titleText">{t("contest_participant_info")}</span>
-        </div>
-        <div className="bannerLink">
-          <a
-            href="https://play.google.com/store/search?q=anystory&c=apps"
-            className="subText"
-            target="_blank"
-          >
-            {t("link_to_her_sns")}&nbsp;
-            <span className="arrowMark">&#8250;</span>
-          </a>
+          <div className="banner_top_text">
+            <SVGBanner1 />
+          </div>
+          <div className="banner_mid_text">
+            <SVGBanner2 />
+            <SVGBanner3 />
+          </div>
+          <div className="banner_bottom_text">
+            <div>
+              <p className="titleText">{t("contest_participant_info")}</p>
+            </div>
+            <div className="bannerLink">
+              <a
+                href="https://play.google.com/store/search?q=anystory&c=apps"
+                className="subText"
+                target="_blank"
+              >
+                {t("link_to_her_sns")}&nbsp;
+                <span className="arrowMark">&#8250;</span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
