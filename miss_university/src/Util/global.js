d9c6b16415d5385d8fe8aadaf5de95ddeee9c6b0
anychatHannah <hannah0175@gmail.com>
2022-11-01 17:24:04 +0900

    새서버 통신 테스트 완료 후 URL 기존도메인으로 연결

--- miss_university/src/Util/global.js
+++ miss_university/src/Util/global.js
@@ -1,4 +1,4 @@
 const g = {};
-// g.base_url = "https://anystorydev.anychat.com:3030/"; //서버 이관 전
-g.base_url = "http://223.130.134.217/"; //서버 이관 후
+g.base_url = "https://anystorydev.anychat.com:3030/"; //서버 이관 전
+// g.base_url = "http://223.130.134.217/"; //서버 이관 후
 export default g;
